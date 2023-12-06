9fd5123d48b0be7b28e36bdb1510635e46f417ab
Chonthicha Jessie Pai-Ngam <chonthicha.pai-ngam@edu.sait.ca>
2023-12-06 11:02:36 -0700

    update FB database

--- Components/Diary.tsx
+++ Components/Diary.tsx
@@ -1,14 +1,54 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity, Button} from 'react-native';
 import PhotoOfTheDay from './PhotoOfTheDay';
 import DateTimePicker from '@react-native-community/datetimepicker';
-
-import {Alert} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import storage, {FirebaseStorageTypes} from '@react-native-firebase/storage';
+import {Asset} from 'react-native-image-picker';
 
 const Diary = () => {
   const [date, setDate] = useState(new Date());
-  const [selfDesc, setSelfDesc] = React.useState('');
-  const [text, setText] = React.useState('');
+  const [selfDesc, setSelfDesc] = useState('');
+  const [text, setText] = useState('');
+  const [image, setImage] = useState<Asset | undefined>();
+  //Unchanging contents while saving diary
+  const [isSaving, setIsSaving] = useState(false);
+
+  const saveDoc = useCallback(
+    async (imageRef: FirebaseStorageTypes.Reference) => {
+      const dateStr = `${date.getFullYear()}-${String(
+        date.getMonth() + 1,
+      ).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+      await firestore().collection('diaries').doc(dateStr).set({
+        date: dateStr,
+        content: text,
+        self_description: selfDesc,
+        photo_ref: imageRef.fullPath,
+      });
+      setIsSaving(false);
+    },
+    [date, text, selfDesc],
+  );
+
+  const saveToFb = useCallback(async () => {
+    if (image?.fileName || !image?.uri) {
+      return;
+    }
+    setIsSaving(true);
+    // TODO: filename should be the date
+    const photoRef = storage().ref(image.fileName);
+    const task = photoRef.putFile(image.uri);
+    task.on('state_changed', event => {
+      if (event.state === 'success') {
+        saveDoc(photoRef);
+      } else if (event.state === 'error') {
+        // TODO: show message to user
+        
+        //Unchanging contents while saving diary
+        setIsSaving(false);
+      }
+    });
+  }, [image]);
 
   return (
     <>
@@ -19,6 +59,8 @@ const Diary = () => {
           value={date}
           mode={'date'}
           is24Hour={true}
+          //Unchanging contents while saving diary
+          disabled={isSaving}
           onChange={e => setDate(new Date(e.nativeEvent.timestamp))}
         />
         <TextInput
@@ -26,7 +68,10 @@ const Diary = () => {
           onChangeText={selfDesc => {
             setSelfDesc(selfDesc);
           }}
+          //Unchanging contents while saving diary
+          editable={!isSaving}
           placeholder="Self Descriptive for Today"
+          
         />
         <TextInput
           onChangeText={text => {
@@ -34,9 +79,14 @@ const Diary = () => {
           }}
           value={text}
           placeholder="Start telling the good things here!!!"
+          //Unchanging contents while saving diary
+          editable={!isSaving}
         />
       </View>
-      <PhotoOfTheDay />
+      <PhotoOfTheDay onPick={setImage} image={image} />
+      <TouchableOpacity onPress={saveToFb}>
+        <Text>Save Diary</Text>
+      </TouchableOpacity>
     </>
   );
 };
--- Components/PhotoOfTheDay.tsx
+++ Components/PhotoOfTheDay.tsx
@@ -1,44 +1,19 @@
-import React, {useState, useEffect, useCallback, useContext} from 'react';
+import React, {useState} from 'react';
 import {Image, View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {Asset, launchImageLibrary} from 'react-native-image-picker';
-import storage, {FirebaseStorageTypes} from '@react-native-firebase/storage';
 
-const PhotoOfTheDay = () => {
-  const [fbStorage, setFbStorage] = useState<FirebaseStorageTypes.Module>();
-  const [image, setImage] = useState<Asset | undefined>(undefined);
+type Props = {
+  image?: {uri?: string};
+  onPick(image: Asset): void;
+};
 
-  useEffect(() => {
-    setFbStorage(storage());
-  }, []);
-
+const PhotoOfTheDay = ({onPick, image}: Props) => {
   const pickImage = async () => {
     const result = await launchImageLibrary({mediaType: 'photo'});
-    console.log(result);
     if (!result.didCancel && result.assets?.[0]?.uri) {
-      setImage(result.assets[0]);
+      onPick(result.assets[0]);
     }
   };
-
-  const photoUpload = useCallback(async () => {
-    if (!fbStorage || !image?.fileName || !image?.uri) {
-      return;
-    }
-    // TODO: filename should be the date
-    const photoRef = fbStorage.ref(image.fileName);
-    const task = photoRef.putFile(image.uri);
-    task.on('state_changed', event => {
-      if (event.state === 'success') {
-        // TODO: write diary (including photoRef.fullPath) to firebase database
-      } else if (event.state === 'error') {
-        // TODO: show message to user
-        setImage(undefined);
-      }
-    });
-  }, [fbStorage, image]);
-
-  useEffect(() => {
-    photoUpload();
-  }, [photoUpload]);
 
   return (
     <View style={imageUploaderStyles.container}>
